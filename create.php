d9554cdb35bc0d117720f09222eadc908487ccfd
freddyalpino <35858296+freddyalpino@users.noreply.github.com>
2019-07-15 15:14:44 +0700

    Add files via upload

--- create.php
+++ create.php
@@ -4,9 +4,9 @@ if (defined('RESTRICTED')) {
     exit('No direct script access allowed!');
 }
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 $admin_login = "";
 
@@ -23,22 +23,41 @@ $admin_login = "{$_SESSION['username']}";
 $admin = $connect->execute("SELECT * FROM tbl_admin WHERE username = '{$admin_login}'");
 
 if (isset($_POST['btn_save'])) {
-    $username   = $_POST['username'];
-    $password   = md5($_POST['password']);
-    $status     = $_POST['status'];
+    $id_user        = $_POST['id_user'];
+    $password       = md5($_POST['password']);
+    $nama_user      = $_POST['nama_user'];
+    $id_fakultas    = $_POST['id_fakultas'];
+    $id_prodi       = $_POST['id_prodi'];
+    $alamat         = $_POST['alamat'];
+    $status         = $_POST['status'];
 
-    if ($username == '') {
-        $error[]    = "Username masih kosong";
+    $checkUser      = $connect->execute("SELECT COUNT(*) AS nidn FROM tbl_user WHERE id_user = '{$id_user}'");
+    $rowUser        = $checkUser->fetch_object();
+
+    if ($id_user == '') {
+        $error[]    = "NIDN/ NIK masih kosong";
+    } elseif (!is_numeric($id_user)) {
+        $error[]    = "NIDN/ NIK tidak valid";
+    } elseif ($rowUser->nidn == 1) {
+        $error[]    = "NIDN/ NIK tidak boleh sama";
     } elseif ($password == '') {
         $error[]    = "Password masih kosong";
-    } elseif (empty($status)) {
-        $error[]    = "Status masih belum dipilih";
+    } elseif ($nama_user == '') {
+        $error[]    = "Nama masih kosong";
+    } elseif (empty($id_fakultas)) {
+        $error[]    = "Fakultas belum dipilih";
+    } elseif (empty($id_prodi)) {
+        $error[]    = "Prodi belum dipilih";
+    } elseif ($alamat == '') {
+        $error[]    = "Alamat masih kosong";
+    } elseif ($status == '') {
+        $error[]    = "Status masih kosong";
     } else {
         try {
-            if ($adminMastering->createAdmins($username, $password, $status)) {
+            if ($adminMastering->createUser($id_user, $password, $nama_user, $id_fakultas, $id_prodi, $alamat, $status)) {
                 
             }
-            $adminMastering->redirect($baseUrl.'admin.php?page=home&action=admins-create&saved');
+            $adminMastering->redirect($baseUrl.'admin.php?page=home&action=user-create&saved');
         } catch(Exception $e) {
             echo $e->getMessage();
         }
@@ -47,5 +66,5 @@ if (isset($_POST['btn_save'])) {
 
 include "apps/views/layouts/header.view.php";
 include "apps/views/admin/menu.view.php";
-include "apps/views/admin/admins/create.view.php";
+include "apps/views/admin/user/create.view.php";
 include "apps/views/layouts/footer.view.php";
